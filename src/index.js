1b14d129ab490e7ac9060a7970ffd213e604271e
coderitual <skowronkow@gmail.com>
2016-07-11 21:04:30 +0200

    Update starting point

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('start')
+const canvas = document.createElement('canvas');
+const ctx = canvas.getContext('2d');
+document.body.appendChild(canvas);
+
+let y = 0;
+
+const update = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.font = '20px Arial';
+  ctx.fillText('0', 0, y);
+  y+=1;
+  if (y > 300) {
+    y = 0;
+  }
+};
+
+const loop = () => {
+  requestAnimationFrame(loop);
+  update();
+};
+
+loop();
