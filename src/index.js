2fedce518419afc29d20f36a6b1f080cff407a60
coderitual <skowronkow@gmail.com>
2016-07-11 21:57:02 +0200

    Add drawMotionText function

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ const ctx = canvas.getContext('2d');
 document.body.appendChild(canvas);
 
 let y = 100;
-let iterations = 40;
+
+const drawMotionText = ({ text, x, y, iterations = 40 }) => {
+  for (let i = 0; i < iterations; i++) {
+    const pos = i - iterations / 2;
+    ctx.globalAlpha = 0.1 - 0.01 * Math.abs(pos);
+    ctx.fillText(text, x, y + pos * 5);
+  }
+};
 
 const update = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -11,13 +18,8 @@ const update = () => {
   ctx.fillStyle = '#FFF';
   ctx.font = 'bold 100px Arial Black';
 
-  for (let i = 0; i < iterations; i++) {
-    const pos = i - iterations / 2;
-    ctx.globalAlpha = 0.1 - 0.01 * Math.abs(pos);
-    ctx.fillText('0', 0, y + pos * 5);
-  }
+  drawMotionText({ text: '0', x: 0, y });
 
-  //y+=5;
   if (y > 300) {
     y = 0;
   }
