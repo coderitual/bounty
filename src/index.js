79e36d887687eff758ec0dbfe60369994ee030f0
coderitual <skowronkow@hotmail.com>
2016-07-30 23:43:43 +0200

    Add filter test

--- src/index.js
+++ src/index.js
@@ -5,11 +5,23 @@ const svg = select('.js-odoo')::append('svg')
   ::attr('width', 400)
   ::attr('height', 300);
 
+const defs = svg::append("defs");
+
+defs::append("filter")
+	::attr("id", "motionFilter")
+	::attr("width", "300%")
+	::attr("x", "-100%")
+	::append("feGaussianBlur")
+	::attr("class", "blurValues")
+	::attr("in", "SourceGraphic")
+	::attr("stdDeviation", "0 8");
+
 const letter = svg::append('text')
   ::attr('y', 100)
   ::style('fill', '#fff')
   ::style('font-size', '85px')
   ::style('text-shadow', '2px 2px 10px rgba(0, 0, 0, 0.5)')
+  ::style('filter', 'url(#motionFilter)')
   ::text('0')
 
 const update = (timestamp) => {
