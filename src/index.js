2dc48d2a866d0dd6cc952ff2a0488a539377f262
coderitual <skowronkow@hotmail.com>
2016-07-31 14:27:51 +0200

    Update roulette

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import loop from './loop';
 import { select, append, attr, style, text } from './selection';
 
+const fontHeight = 85;
+const height = 100;
+
 const createDigitRoulette = (svg) => {
   const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   const roulette = svg::append('g');
   digits.forEach((el, i) => {
     roulette::append('text')
-      ::attr('y', -i * 100)
+      ::attr('y', -i * height)
       ::style('fill', '#fff')
-      ::style('font-size', '85px')
-      ::style('text-shadow', '2px 2px 10px rgba(0, 0, 0, 0.5)')
+      ::style('font-size', `${fontHeight}px`)
       ::text(el)
   });
   return roulette;
@@ -17,7 +19,7 @@ const createDigitRoulette = (svg) => {
 
 const svg = select('.js-odoo')::append('svg')
   ::attr('width', 800)
-  ::attr('height', 800);
+  ::attr('height', height);
 
 const defs = svg::append('defs');
 
@@ -28,14 +30,16 @@ defs::append('filter')
 	::append('feGaussianBlur')
 	::attr('class', 'blurValues')
 	::attr('in', 'SourceGraphic')
-	::attr('stdDeviation', '0 1');
+	::attr('stdDeviation', '0 0');
 
 const digit = createDigitRoulette(svg)
   ::style('filter', 'url(#motionFilter)')
 
 const update = (timestamp) => {
-  digit::attr('transform', `translate(0, ${(timestamp / 10) % 1000})`)
-  select('#motionFilter .blurValues')::attr('stdDeviation', `0 ${timestamp / 1000}`);
+  const offset = fontHeight + (height - fontHeight) / 2;
+  const y = offset + 0 / 10 % 1000;
+  digit::attr('transform', `translate(0, ${y})`)
+  //select('#motionFilter .blurValues')::attr('stdDeviation', `0 ${timestamp / 1000}`);
 };
 
 loop(update).start();
