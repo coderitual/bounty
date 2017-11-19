5f589e8ca514534c84bf4d352aaabb84e018e9c7
coderitual <skowronkow@hotmail.com>
2017-11-20 00:56:46 +0100

    fix(#39): center character in roulette to avoid strange kerning efffect

--- src/bounty.js
+++ src/bounty.js
@@ -1,87 +1,99 @@
-import loop from './loop';
-import { select, append, attr, style, text } from './selection';
-import transition from './transition';
+import loop from "./loop";
+import { select, append, attr, style, text } from "./selection";
+import transition from "./transition";
 
 const DIGITS_COUNT = 10;
 const ROTATIONS = 3;
 
 const createDigitRoulette = (svg, fontSize, lineHeight, id) => {
   const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-  const roulette = svg::append('g')
-    ::attr('id', `digit-${id}`)
-    ::style('filter', `url(#motionFilter-${id})`);
+  const roulette = svg
+    ::append("g")
+    ::attr("id", `digit-${id}`)
+    ::style("filter", `url(#motionFilter-${id})`);
 
   digits.forEach((el, i) => {
-    roulette::append('text')
-      ::attr('y', -i * fontSize * lineHeight)
+    roulette
+      ::append("text")
+      ::attr("y", -i * fontSize * lineHeight)
       ::text(el);
   });
+
   return roulette;
 };
 
-const createCharacter = (svg, el, fontSize) => svg
-  ::append('g')
-  ::append('text')
-  ::style('filter', `url(#createShadowFailFilter)`)
-  ::text(el);
+const createCharacter = (svg, el, fontSize) =>
+  svg
+    ::append("g")
+    ::append("text")
+    ::style("filter", `url(#createShadowFailFilter)`)
+    ::text(el);
 
-const createFilter = (defs, id) => defs::append('filter')
-	::attr('id', `motionFilter-${id}`)
-	::attr('width', '300%')
-	::attr('x', '-100%')
-	::append('feGaussianBlur')
-    ::attr('class', 'blurValues')
-    ::attr('in', 'SourceGraphic')
-    ::attr('stdDeviation', '0 0');
+const createFilter = (defs, id) =>
+  defs
+    ::append("filter")
+    ::attr("id", `motionFilter-${id}`)
+    ::attr("width", "300%")
+    ::attr("x", "-100%")
+    ::append("feGaussianBlur")
+    ::attr("class", "blurValues")
+    ::attr("in", "SourceGraphic")
+    ::attr("stdDeviation", "0 0");
 
-const createShadowFailFilter = (defs) => defs::append('filter')
-	::attr('id', `createShadowFailFilter`)
-	::attr('width', '300%')
-	::attr('x', '-100%')
-  	::append('feGaussianBlur')
-    ::attr('stdDeviation', '0 0');
+const createShadowFailFilter = defs =>
+  defs
+    ::append("filter")
+    ::attr("id", `createShadowFailFilter`)
+    ::attr("width", "300%")
+    ::attr("x", "-100%")
+    ::append("feGaussianBlur")
+    ::attr("stdDeviation", "0 0");
 
-const createGradient = (defs, id) => defs::append('linearGradient')
-  ::attr('id', `gradient-${id}`)
-  ::attr('x1', '0%')
-  ::attr('y1', '0%')
-  ::attr('x2', '0%')
-  ::attr('y2', '100%')
-  ::append('stop')
-    ::attr('offset', '0')
-    ::attr('stop-color', 'white')
-    ::attr('stop-opacity', '0')
-  ::select(`#gradient-${id}`)
-  ::append('stop')
-    ::attr('offset', '0.2')
-    ::attr('stop-color', 'white')
-    ::attr('stop-opacity', '1')
-  ::select(`#gradient-${id}`)
-  ::append('stop')
-    ::attr('offset', '0.8')
-    ::attr('stop-color', 'white')
-    ::attr('stop-opacity', '1')
-  ::select(`#gradient-${id}`)
-  ::append('stop')
-    ::attr('offset', '1')
-    ::attr('stop-color', 'white')
-    ::attr('stop-opacity', '0');
+const createGradient = (defs, id) =>
+  defs
+    ::append("linearGradient")
+    ::attr("id", `gradient-${id}`)
+    ::attr("x1", "0%")
+    ::attr("y1", "0%")
+    ::attr("x2", "0%")
+    ::attr("y2", "100%")
+    ::append("stop")
+    ::attr("offset", "0")
+    ::attr("stop-color", "white")
+    ::attr("stop-opacity", "0")
+    ::select(`#gradient-${id}`)
+    ::append("stop")
+    ::attr("offset", "0.2")
+    ::attr("stop-color", "white")
+    ::attr("stop-opacity", "1")
+    ::select(`#gradient-${id}`)
+    ::append("stop")
+    ::attr("offset", "0.8")
+    ::attr("stop-color", "white")
+    ::attr("stop-opacity", "1")
+    ::select(`#gradient-${id}`)
+    ::append("stop")
+    ::attr("offset", "1")
+    ::attr("stop-color", "white")
+    ::attr("stop-opacity", "0");
 
-const createMask = (defs, id) => defs::append('mask')
-  ::attr('id', `mask-${id}`)
-  ::append('rect')
-  ::attr('x', 0)
-  ::attr('y', 0)
-  ::attr('width', '100%')
-  ::attr('height', '100%')
-  ::attr('fill', `url(#gradient-${id})`);
+const createMask = (defs, id) =>
+  defs
+    ::append("mask")
+    ::attr("id", `mask-${id}`)
+    ::append("rect")
+    ::attr("x", 0)
+    ::attr("y", 0)
+    ::attr("width", "100%")
+    ::attr("height", "100%")
+    ::attr("fill", `url(#gradient-${id})`);
 
 const setViewBox = (svg, width, height) => {
-  svg::attr('width', width)
-  svg::attr('height', height)
-  svg::attr('viewBox', `0 0 ${width} ${height}`)
-  svg::style('overflow', 'hidden');
-}
+  svg::attr("width", width);
+  svg::attr("height", height);
+  svg::attr("viewBox", `0 0 ${width} ${height}`);
+  svg::style("overflow", "hidden");
+};
 
 export default ({
   el,
@@ -102,34 +114,35 @@ export default ({
   let canvasWidth = 0;
   const canvasHeight = fontSize * lineHeight + marginBottom;
 
-  element.innerHTML = '';
-  const root = element::append('svg');
-  const svg = root::append('svg')::attr('mask', `url(#mask-${salt})`)
-  const defs = root::append('defs');
+  element.innerHTML = "";
+  const root = element::append("svg");
+  const svg = root::append("svg")::attr("mask", `url(#mask-${salt})`);
+  const defs = root::append("defs");
   createGradient(defs, salt);
   createMask(defs, salt);
   createShadowFailFilter(defs);
 
   const prepareValues = (value, secondValue) => {
     const values = String(value)
-      .replace(/ /g, '\u00a0')
-      .split('');
+      .replace(/ /g, "\u00a0")
+      .split("");
 
     const digitIndex = String(value).search(/\d/);
     while (secondValue.length > values.length) {
-      const char = secondValue[secondValue.length - values.length - 1 + digitIndex];
-      values.splice(digitIndex, 0, isNaN(parseInt(char, 10)) ? char : '0');
+      const char =
+        secondValue[secondValue.length - values.length - 1 + digitIndex];
+      values.splice(digitIndex, 0, isNaN(parseInt(char, 10)) ? char : "0");
     }
     return values;
   };
 
-  const initialString = String(initialValue || '0');
+  const initialString = String(initialValue || "0");
   const values = prepareValues(String(value), initialString);
   const initial = prepareValues(initialString, String(value));
 
   const chars = values.map((char, i) => {
     const id = `${i}-${salt}`;
-    if(isNaN(parseInt(char, 10)) || isNaN(parseInt(initial[i], 10))) {
+    if (isNaN(parseInt(char, 10)) || isNaN(parseInt(initial[i], 10))) {
       return {
         isDigit: false,
         node: createCharacter(svg, char, fontSize),
@@ -144,7 +157,10 @@ export default ({
         filter: createFilter(defs, id),
         value: Number(char),
         initial: Number(initial[i]),
-        offset: { x: 0, y: offset + Number(initial[i]) * (fontSize * lineHeight) }
+        offset: {
+          x: 0,
+          y: offset + Number(initial[i]) * (fontSize * lineHeight)
+        }
       };
     }
   });
@@ -153,33 +169,57 @@ export default ({
   const digits = chars.filter(char => char.isDigit);
   digits.forEach((digit, i) => {
     const sourceDistance = digit.initial * (fontSize * lineHeight);
-    const targetDistance = (ROTATIONS * DIGITS_COUNT + digit.value) * (fontSize * lineHeight);
+    const targetDistance =
+      (ROTATIONS * DIGITS_COUNT + digit.value) * (fontSize * lineHeight);
     const digitTransition = transition({
       from: sourceDistance,
       to: targetDistance,
       delay: (digits.length - 1 - i) * letterAnimationDelay + animationDelay,
       step(value) {
-        digit.offset.y = offset + value % ((fontSize * lineHeight) * DIGITS_COUNT);
-        digit.node::attr('transform', `translate(${digit.offset.x}, ${digit.offset.y})`);
+        digit.offset.y =
+          offset + value % (fontSize * lineHeight * DIGITS_COUNT);
+        digit.node::attr(
+          "transform",
+          `translate(${digit.offset.x}, ${digit.offset.y})`
+        );
         const filterOrigin = (sourceDistance + targetDistance) / 2;
-        const motionValue = (Math.abs(Math.abs(value - filterOrigin) - filterOrigin) - sourceDistance) / 100;
-        digit.filter::attr('stdDeviation', `0 ${motionValue}`);
+        const motionValue =
+          (Math.abs(Math.abs(value - filterOrigin) - filterOrigin) -
+            sourceDistance) /
+          100;
+        digit.filter::attr("stdDeviation", `0 ${motionValue}`);
       },
-      end: i === 0 ? () => cancelAnimation() : (e) => e
+      end: i === 0 ? () => cancelAnimation() : e => e
     });
     transitions.push(digitTransition);
   });
 
-  const update = (timestamp) => {
+  const update = timestamp => {
     canvasWidth = 0;
     chars.forEach(char => {
       const { width } = char.node.getBBox();
-      char.offset.x = canvasWidth;
+
+      if (char.offset.x !== canvasWidth) {
+        char.offset.x = canvasWidth;
+        // set proper kerning for proportional fonts
+        if (char.isDigit) {
+          [...char.node.children].forEach(element => {
+            const { width: letterWidth } = element.getBBox();
+            const offset = (width - letterWidth) / 2;
+            console.log(offset)
+            element.setAttribute('x', offset)
+          });
+        }
+      }
+
       canvasWidth += width + letterSpacing;
     });
 
     chars.forEach(char => {
-      char.node::attr('transform', `translate(${char.offset.x}, ${char.offset.y})`);
+      char.node::attr(
+        "transform",
+        `translate(${char.offset.x}, ${char.offset.y})`
+      );
     });
 
     setViewBox(root, canvasWidth, canvasHeight);
